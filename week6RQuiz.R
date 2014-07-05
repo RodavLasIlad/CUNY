937b37440ceaf2092346381d49db790936c99410
Brett Burk <brettmburk@gmail.com>
2014-07-05 08:32:39 -0400

    week6RQuiz

---
+++ week6RQuiz.R
@@ -0,0 +1,7 @@
+#Week 6 Quiz
+myData <- read.csv("C:/Users/Brett/Dropbox/CUNY/pizza-store-data.csv", header=T)
+summary(myData)
+# dat - date in yyyy-mm-dd format
+# store: one of four store names
+# pizzas: presumably the amount of pizzas sold
+# customers: presumably the amount of customers served
