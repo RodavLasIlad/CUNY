4be33d35e0ac2f52c1896f075c31658cd0a34fe0
Brett Burk <brettmburk@gmail.com>
2014-07-30 20:09:04 -0400

    knitr, rmd, etc.

--- week9QuantDetSgn.R
+++ week9QuantDetSgn.R
@@ -8,3 +8,8 @@ determinant <- function(A){
     return(do.call(sum, lapply(1:ncol(A), function(i) sgn(i)*A[1,i] * determinant(A[-1,-i,drop=F])))) # finds the sgn of each number, multiplies it by the current value in row 1, then finds the determinant of the rest of the row, and finally it sums all the values
   }
 }
+
+noninvert <- matrix(c(1,2,3,4,5,6,7,8,9), byrow=T, ncol=3)
+invert <- matrix(c(1,2,3,0,2,3,0,0,4), byrow=T, ncol=3)
+determinant(noninvert)
+determinant(invert)
