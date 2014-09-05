83d6806717a872d28aaf72abbd65859f5b4703de
Brett Burk <brettmburk@gmail.com>
2014-09-05 07:11:01 -0400

    Added proper question tags

--- 607/bburk607Assign2.R
+++ 607/bburk607Assign2.R
@@ -1,28 +1,28 @@
-# 1.a
+##### Question 1.a #####
 queue <- c("James", "Mary", "Steve", "Alex", "Patricia")
 
-# 1.b
+##### Question 1.b #####
 queue <- c(queue, "Harold")
 
-# 1.c
+##### Question 1.c #####
 queue <- queue[2:length(queue)]
 
-# 1.d (Making it nearly as complicated as possible)
+##### Question 1.d ##### (Making it nearly as complicated as possible)
 queue[c(1:(which(queue == "Steve")-1), which(queue == "Patricia"), (which(queue == "Steve"):length(queue))[-((length(queue) - which(queue == "Steve")))])]
 
-# 1.e
+##### Question 1.e #####
 queue <- subset(queue, queue != "Harold")
 
-# 1.f
+##### Question 1.f #####
 queue <- subset(queue, queue != "Alex")
 
-# 1.g
+##### Question 1.g #####
 which(queue == "Patricia")
 
-# 1.h
+##### Question 1.h #####
 length(queue)
 
-# 2
+##### Question 2   #####
 quadratic <- function(a,b,c){
   if (a == 0){
     return("No Solution")
@@ -39,11 +39,11 @@ quadratic <- function(a,b,c){
   return(c(pos, neg))
 }
 
-# 3
+##### Question 3   #####
 one.to.onek <- c(1:1000)
 length(one.to.onek[intersect(intersect(which(one.to.onek %% 3 != 0), which(one.to.onek %% 7 != 0)), which(one.to.onek %% 11 != 0))])
 
-# 4
+##### Question 4   #####
 pythag.trip <- function(a, b, c){
   big <- max(c(a,b,c))^2
   if(sum((c(a,b,c))[-which.max(c(a,b,c))]^2) == big){
--- 607/bburk607Quiz2.R
+++ 607/bburk607Quiz2.R
@@ -1,19 +1,19 @@
-# 1
+##### Question 1 #####
 vec <- c(1:7, 6:14, 17:20)
 
-# 2
+##### Question 2 #####
 vec.char <- as.character(vec)
 
-# 3
+##### Question 3 #####
 vec.fact <- as.factor(vec)
 
-#4
+##### Question 4 #####
 length(unique(vec.fact))
 
-# 5
+##### Question 5 #####
 vec.math <- 3 * (vec)^2 -4*(vec) + 1
 
-# 6
+##### Question 6 #####
 leastSqReg <- function(X, y){
   return (solve(t(X) %*% X) %*% t(X) %*% y)
 }
@@ -21,51 +21,51 @@ X <- matrix(c(1,5,8,1,4,9,1,6,4,1,2,7,1,3,4,1,2,9,1,7,6,1,8,4), ncol=3, byrow=T)
 y <- c(45.2, 46.9, 31.0, 35.3, 25.0, 43.1, 41, 35.1)
 leastSqReg(X, y)
 
-# 7
+##### Question 7 #####
 list.named <- list(first=1, second=2, third=3)
 
-# 8
+##### Question 8 #####
 df.char <- c(letters[1:10])
 df.fact <- as.factor(rep(c("low", "mid", "high"), length.out=10))
 df.numb <- c(1:10)
 df.date <- as.Date(c(Sys.Date() - 0:9))
 df <- data.frame(df.char, df.fact, df.numb, df.date)
 
-# 9
+##### Question 9 #####
 df.fact <- as.factor(rep(c("low", "mid", "high"), length.out=10))
 levels(df.fact) <- c(levels(df.fact), "highest")
 df.fact[length(df.fact) + 1] <- "highest"
 
-# 10
+##### Question 10 #####
 temps <- read.csv("temperatures.csv")
 
-# 11
+##### Question 11 #####
 msmts <- read.table("C:\\Other\\measurements.txt", sep = "\t")
 
-# 12
+##### Question 12 #####
 library(RCurl)
 page <- getURL("http://wordpress.org/plugins/about/readme.txt")
 page <- read.table(tc <- textConnection(page), sep="|")
 close(tc)
 
-# 13
+##### Question 13 #####
 factorial <- 1
 for (i in 12:1){
   factorial <- factorial * i
 }
 
-# 14
+##### 14 #####
 balance <- 1500
 for (i in (1:(6*12))){
   balance <- balance * 1.0324
 }
 balance <- round(balance, digits=2)
 
-# 15
+##### 15 #####
 vec.third <- c(10:30)
 sum(vec.third[seq(0, length(vec.third), 3)])
 
-# 16
+##### 16 #####
 x <- 2
 x.sum <- 0
 for(i in 1:10){
@@ -73,7 +73,7 @@ for(i in 1:10){
   print(x.sum)
 }
 
-# 17
+##### 17 #####
 x.sum <- 0
 i <- 1
 while(i < 11){
@@ -81,18 +81,19 @@ while(i < 11){
   i <- i+1
 }
 
-# 18
+##### 18 #####
 sum(x^(1:10))
 
-# 19
+##### 19 #####
 vector.by.fives <- seq(20, 50, 5)
 
-#20
+##### 20 #####
 example.ten <- c(rep("example", 10))
 
-#21
+##### 21 #####
 quadratic <- function(a,b,c){
   pos <- (-b + sqrt(b^2 - 4 * a * c))/(2*a)
   neg <- (-b - sqrt(b^2 - 4 * a * c))/(2*a)
   return(c(pos, neg))
 }
+quadratic(1,-3, -4)
