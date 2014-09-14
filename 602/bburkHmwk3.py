aff482272ee399cd58867d102fe78602c5beb6af
Brett Burk <brettmburk@gmail.com>
2014-09-14 14:12:38 -0400

    Added error checking to 602 hmwk 3

--- 602/bburkHmwk3.py
+++ 602/bburkHmwk3.py
@@ -9,14 +9,30 @@ with open('cars.data.csv', 'rb') as csvfile:
     for row in reader:
         vals.append(row)
 
-"""
-   buying       v-high, high, med, low
-   maint        v-high, high, med, low
-   doors        2, 3, 4, 5-more
-   persons      2, 4, more
-   lug_boot     small, med, big
-   safety       low, med, high
-"""
+##### Cleaning the data #####
+buying = ("vhigh", "high", "med", "low")
+maint = ("vhigh", "high", "med", "low")
+doors = ("2", "3", "4", "5more")
+persons =("2", "4", "more")
+lug_boot = ("small", "med", "big")
+safety = ("low", "med", "high")
+err = []
+for i in range(len(vals)):
+    if (vals[i][0] not in buying) or (vals[i][1] not in maint) or (vals[i][2] not in doors) or (vals[i][3] not in persons) or (vals[i][4] not in lug_boot) or (vals[i][5] not in safety):
+        err.append(i)
+##### Removing rows that contain erroneus data #####
+if err:
+    print "The following rows contain erroneus data and have been removed:"
+    err.reverse()
+    for i in err:
+        print "Row #%s" % (i)
+        print(vals[i])
+        vals.pop(i)
+else:
+    print "No errors found in the data"
+
+print "\n" * 3        
+
 ##### Question 2 #####
 ##### Part a #####
 print "-" * 15 + "Safety descending (top 10)" +"-" * 15
@@ -25,16 +41,14 @@ print "%s \t %s \t %s \t %s \t %s \t %s \t %s" % tuple(header)
 print "-" * 56
 
 levelsSafety = {"high": 10, "med": 5, "low": 1}
-try:
-    valsSafety = sorted(vals, key = lambda e: levelsSafety[e[5]], reverse=True)[1:10]
-    for i in valsSafety:
-        print '\t '.join(i)
-    print "\n" * 3
-except:
+valsSafety = sorted(vals, key = lambda e: levelsSafety[e[5]], reverse=True)[1:10]
+for i in valsSafety:
+    print '\t '.join(i)
+print "\n" * 3
     
 
 ##### Part b #####
-print "-" * 15 + "Maintenance ascending (bot 10)" +"-" * 15
+print "-" * 13 + "Maintenance ascending (bot 10)" +"-" * 13
 header = ["buying", "maint", "doors", "ppl", "lug", "safe", "class"]
 print "%s \t %s \t %s \t %s \t %s \t %s \t %s" % tuple(header)
 print "-" * 56
@@ -45,13 +59,14 @@ valsMaint = sorted(vals, key = lambda e: levelsMaint[e[1]])[-15:]
 for i in valsMaint:
     print '\t '.join(i)
     
+print "\n" * 3
 ##### Part c #####
 regMatches = []
 for i in vals:
     if re.match("v?high", i[0]) and re.match("v?high", i[1]) and re.match("v?high", i[5]):
         regMatches.append(i)
         
-print "-" * 15 + "High/VHigh in buying, maint, and safety by doors ascending" +"-" * 15
+print "High/VHigh in buying, maint, and safety by doors ascending"
 header = ["buying", "maint", "doors", "ppl", "lug", "safe", "class"]
 print "%s \t %s \t %s \t %s \t %s \t %s \t %s" % tuple(header)
 print "-" * 56
@@ -60,6 +75,7 @@ levelsReg = {"2":2, "3":3, "4":4, "5more":5}
 valsReg = sorted(regMatches, key = lambda e: levelsReg[e[2]])
 for i in valsReg:
     print '\t'.join(i)
+print "\n" * 3
 
 ##### Part d #####
 specMatches = []
